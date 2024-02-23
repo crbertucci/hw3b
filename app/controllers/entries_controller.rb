6b6145a9cc2ff42c413880cc707ec3e22e8f8916
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-02-23 19:45:09 +0000

    2231345

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,4 +3,18 @@ class EntriesController < ApplicationController
   def index 
     render :inline => "<h1> Entries </h1>"
   end
+
+  def new 
+    @entry = Entry.new 
+
+    @entry.place_id = params["company_id"]
+end
+
+def create
+  @entry = Entry.new
+  @entry["title"] = params["title"]
+  @entry["description"] = params["posted_on"]
+  @entry.save
+  redirect_to "/places/#{@entry["place_id"]}"
+end
 end
