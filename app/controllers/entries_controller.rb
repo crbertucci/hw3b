2193db9508ee4ddb30ddb526e395f101e889f2fe
Chris Bertucci <christopher.bertucci@kellogg.northwestern.edu>
2024-02-24 16:25:27 +0000

    Final

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,19 +1,15 @@
 class EntriesController < ApplicationController
 
-  def index 
-    render :inline => "<h1> Entries </h1>"
-  end
-
   def new 
-    @entry = Entry.new 
-
-    @entry.place_id = params["company_id"]
+    @place = Place.find_by({ "id" => params["place_id"] })
 end
 
 def create
   @entry = Entry.new
   @entry["title"] = params["title"]
-  @entry["description"] = params["posted_on"]
+  @entry["description"] = params["description"]
+  @entry["posted_on"] = params["posted_on"]
+  @entry["place_id"] = params["place_id"]
   @entry.save
   redirect_to "/places/#{@entry["place_id"]}"
 end
